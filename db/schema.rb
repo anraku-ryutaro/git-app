f659d64e30f21261999a6522d7fa96a812f045f5
anraku-ryutaro <anrakuryutaro@gmail.com>
2019-09-30 16:36:30 +0900

    create pull request

---
+++ db/schema.rb
@@ -0,0 +1,5 @@
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
